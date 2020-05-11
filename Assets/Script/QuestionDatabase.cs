9b7704dbac7e8e7f09bd87f8100dc8d8b7a62230
Denise <denisevandenberg@outlook.com>
2020-05-11 18:31:41 +0200

    init commit

---
+++ Assets/Script/QuestionDatabase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionDatabase : MonoBehaviour
+{
+    public List<Question> questions;
+    [SerializeField]
+    UI UI;
+    int index;
+
+    private void Start()
+    {
+        FirstQuestion();
+    }
+
+    public void FirstQuestion()
+    {
+        UI.UpdateQuestionText(0);
+    }
+
+    public void NextQuestion()
+    {
+            UI.correctPanel.SetActive(false);
+            UI.answerInputField.text = "";
+            index++;
+            UI.UpdateQuestionText(index);
+    }
+
+    public void CheckAnswer()
+    {
+        if (UI.answerInputField.text == questions[index].answer)
+            UI.correctPanel.SetActive(true);
+        else
+            StartCoroutine(WrongPanelRoutine());
+    }
+
+    IEnumerator WrongPanelRoutine()
+    {
+        UI.wrongPanel.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        UI.wrongPanel.SetActive(false);
+    }
+}
---
+++ Assets/Script/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI questionText;
+    [SerializeField]
+    public TMP_InputField answerInputField;
+    [SerializeField]
+    public GameObject wrongPanel;
+    [SerializeField]
+    public GameObject correctPanel;
+    [SerializeField]
+    public GameObject completePanel;
+    [SerializeField]
+    QuestionDatabase questionDatabase;
+
+    public void UpdateQuestionText(int questionIndex)
+    {
+        questionText.text = questionDatabase.questions[questionIndex].question;
+    }
+}
